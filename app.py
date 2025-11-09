2868e588a7d10a94baf9ab1c8eb6951cf0fa542a
root <root@ip-172-31-15-22.ap-south-1.compute.internal>
2025-11-09 08:00:46 +0000

    python code updated

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ def factorial(n):
 
 num = 5
 print(f"The factorial of {num} is: {factorial(num)}")
+
+code-2
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print(f"{number} is an even number.")
+else:
+    print(f"{number} is an odd number.")
